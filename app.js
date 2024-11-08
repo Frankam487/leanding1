08e4f0e2c4e4dcd4f952672a134fd36fb31ca2e4
Frankam487 <fkamgang06@gmail.com>
2024-11-08 14:47:59 -0800

    test

--- app.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById('articles').addEventListener('change', function() {
-    const url = this.value; 
-    if (url) {
-        window.location.href = url; 
-    }
-});
--- bdConf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost", "root", "", "ecommerce");
-
-if(!$con){
-  die("Error " . mysqli_error());
-}
